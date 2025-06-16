fdd9e8781c2fe17a11a893ba80c6b71fdd44213f
bitWarrior <bitWarrior@protonmail.com>
2025-06-15 22:32:48 -0700

    Updated project.cmake for PiCamera component

--- project.cmake
+++ project.cmake
@@ -8,4 +8,5 @@ add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Sv
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Svc/TempSensor/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Svc/COMMS/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Svc/PowerManager/")
+add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Svc/PiCamera/")
 add_fprime_subdirectory("${CMAKE_CURRENT_LIST_DIR}/lib/fprime-amsat-sim/amsat/Drv/GpioDriver/")
